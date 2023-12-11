3516f57d8699ff7c5bcd4ef393373c72599d8561
Egor Eremeev <egor@eremeev.me>
2023-12-11 19:32:27 +0400

    remove run.sh use canopy start instead

--- run.sh
+++
@@ -1,7 +0,0 @@
-# Load environment variables from .env file and export them
-set -a # automatically export all variables
-source .env
-set +a # stop automatically exporting
-
-# Existing command
-uvicorn canopy_server.app:app --host localhost --port 8000 --reload
